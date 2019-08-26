02ece882040f9b9fcdc3e007fff76904b6143887
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2019-08-26 20:54:03 +0200

    Starting new goods transfers

--- src/pages/GoodsPage.svelte
+++ src/pages/GoodsPage.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from 'svelte';
     import { goods } from '../stores/goods.js';
     import { ships, shipTotals } from '../stores/ships.js';
     import { merchant } from '../stores/merchant.js';
@@ -6,9 +7,43 @@
     import Table from '../components/Table.svelte';
     import Button from '../components/Button.svelte';
     import { goodsInfo } from '../utils/goods';
+    import RangeSlider from '../components/RangeSlider.svelte';
+    import cloneDeep from 'lodash-es/cloneDeep';
+
+    $: tempGoods = cloneDeep($goods);
+    $: tempDoubloons = $merchant.doubloons;
+
+    const changeGoods = (item, quantity) => {
+        quantity = parseInt(quantity, 10);
+
+        const newTempGoods = cloneDeep(tempGoods);
+        newTempGoods[item] = quantity;
+        tempGoods = newTempGoods;
+
+        if (quantity > $goods[item]) {
+            // Buy
+            const price = goodsInfo[item].price;
+            tempDoubloons -= price;
+        } else if (quantity < $goods[item]) {
+            // Sell
+            const worth = goodsInfo[item].worth;
+            tempDoubloons += worth;
+        }
+    };
 
     let showModal = false;
 </script>
+
+<style>
+    .slider-container {
+        padding: 1em 0;
+    }
+    .slider-container > div {
+        background: #fff;
+        padding: 0.5em;
+        margin-bottom: 1.5em;
+    }
+</style>
 
 <div class="app-goods">
     <h2>Goods</h2>
@@ -36,6 +71,26 @@
     <div>
         <Button on:click={() => (showModal = true)}>🥫 Buy and sell goods</Button>
         <Button on:click={() => merchant.addDoubloons(1000)}>💰 Magic Money</Button>
+    </div>
+
+    <div class="slider-container">
+        {#each Object.keys(tempGoods) as item}
+            <div>
+                <RangeSlider
+                    value={tempGoods[item]}
+                    max={tempGoods[item] + 500}
+                    on:input={e => changeGoods(item, e.target.value)}
+                    label="{goodsInfo[item].icon}
+                    {goodsInfo[item].name} ({tempGoods[item]}
+                    {goodsInfo[item].suffix})" />
+            </div>
+        {/each}
+
+        <div>Doubloons: {tempDoubloons}</div>
+
+        <div>
+            <Button>Buy / Sell</Button>
+        </div>
     </div>
 
     {#if showModal}
