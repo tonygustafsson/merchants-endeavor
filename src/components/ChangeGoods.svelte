4fb984d8e1fcb54d197ca239cc2aacdcf86db6ca
Tony Gustafsson <mail@tonyg.se>
2019-09-01 14:46:00 +0200

    Make own component

---
+++ src/components/ChangeGoods.svelte
@@ -0,0 +1,87 @@
+<script>
+    import { goods } from '../stores/goods.js';
+    import { merchant } from '../stores/merchant.js';
+    import Button from '../components/Button.svelte';
+    import { goodsInfo } from '../utils/goods';
+    import RangeSlider from '../components/RangeSlider.svelte';
+    import cloneDeep from 'lodash-es/cloneDeep';
+
+    $: tempGoods = cloneDeep($goods);
+    $: tempDoubloons = $merchant.doubloons;
+
+    const calculateDoubloons = () => {
+        let totalCost = 0;
+
+        Object.keys(tempGoods).forEach(goodsItem => {
+            // Loop through all goods items and calculate the cost/profit
+            const currentQuantity = $goods[goodsItem];
+            const newQuantity = tempGoods[goodsItem];
+            const worth = goodsInfo[goodsItem].price;
+
+            if (newQuantity > currentQuantity) {
+                // Buying item
+                totalCost += (newQuantity - currentQuantity) * worth;
+            } else if (currentQuantity > newQuantity) {
+                // Selling item
+                totalCost -= (currentQuantity - newQuantity) * worth;
+            }
+        });
+
+        tempDoubloons = $merchant.doubloons - totalCost;
+    };
+
+    const changeGoods = (item, quantity) => {
+        // Change the item quantity
+        const newTempGoods = cloneDeep(tempGoods);
+        newTempGoods[item] = parseInt(quantity, 10);
+
+        tempGoods = newTempGoods;
+        calculateDoubloons();
+    };
+
+    const makeTransaction = () => {
+        merchant.setDoubloons(tempDoubloons);
+        goods.updateAll(tempGoods);
+    };
+
+    const resetGoods = () => {
+        tempGoods = cloneDeep($goods);
+        calculateDoubloons();
+    };
+
+    let showModal = false;
+</script>
+
+<style>
+    .slider-container {
+        padding: 1em 0;
+    }
+    .slider-container > div {
+        background: #fff;
+        padding: 0.5em;
+        margin-bottom: 1.5em;
+    }
+    .warning {
+        color: red;
+        font-weight: bold;
+    }
+</style>
+
+<div class="slider-container">
+    {#each Object.keys(tempGoods) as item}
+        <div>
+            <RangeSlider
+                value={tempGoods[item]}
+                max={tempGoods[item] + 500}
+                on:input={e => changeGoods(item, e.target.value)}
+                label="{goodsInfo[item].icon}
+                {goodsInfo[item].name} ({tempGoods[item]}
+                {goodsInfo[item].suffix})" />
+        </div>
+    {/each}
+
+    <p class:warning={tempDoubloons < 0}>Doubloons afterwards: {tempDoubloons} dbl</p>
+
+    <Button on:click={makeTransaction} disabled={tempDoubloons < 0}>💰 Make transaction</Button>
+    <Button on:click={resetGoods}>&times; Reset</Button>
+</div>
--- src/pages/GoodsPage.svelte
+++ src/pages/GoodsPage.svelte
@@ -1,75 +1,14 @@
 <script>
-    import { onMount } from 'svelte';
     import { goods } from '../stores/goods.js';
     import { ships, shipTotals } from '../stores/ships.js';
     import { merchant } from '../stores/merchant.js';
-    import Modal from '../components/Modal.svelte';
     import Table from '../components/Table.svelte';
     import Button from '../components/Button.svelte';
+    import ChangeGoods from '../components/ChangeGoods.svelte';
     import { goodsInfo } from '../utils/goods';
-    import RangeSlider from '../components/RangeSlider.svelte';
-    import cloneDeep from 'lodash-es/cloneDeep';
 
-    $: tempGoods = cloneDeep($goods);
-    $: tempDoubloons = $merchant.doubloons;
-
-    const calculateDoubloons = () => {
-        let totalCost = 0;
-
-        Object.keys(tempGoods).forEach(goodsItem => {
-            // Loop through all goods items and calculate the cost/profit
-            const currentQuantity = $goods[goodsItem];
-            const newQuantity = tempGoods[goodsItem];
-            const worth = goodsInfo[goodsItem].price;
-
-            if (newQuantity > currentQuantity) {
-                // Buying item
-                totalCost += (newQuantity - currentQuantity) * worth;
-            } else if (currentQuantity > newQuantity) {
-                // Selling item
-                totalCost -= (currentQuantity - newQuantity) * worth;
-            }
-        });
-
-        tempDoubloons = $merchant.doubloons - totalCost;
-    };
-
-    const changeGoods = (item, quantity) => {
-        // Change the item quantity
-        const newTempGoods = cloneDeep(tempGoods);
-        newTempGoods[item] = parseInt(quantity, 10);
-
-        tempGoods = newTempGoods;
-        calculateDoubloons();
-    };
-
-    const makeTransaction = () => {
-        merchant.setDoubloons(tempDoubloons);
-        goods.updateAll(tempGoods);
-    };
-
-    const resetGoods = () => {
-        tempGoods = cloneDeep($goods);
-        calculateDoubloons();
-    };
-
-    let showModal = false;
+    $: showChangeGoods = false;
 </script>
-
-<style>
-    .slider-container {
-        padding: 1em 0;
-    }
-    .slider-container > div {
-        background: #fff;
-        padding: 0.5em;
-        margin-bottom: 1.5em;
-    }
-    .warning {
-        color: red;
-        font-weight: bold;
-    }
-</style>
 
 <div class="app-goods">
     <h2>Goods</h2>
@@ -95,65 +34,11 @@
     </Table>
 
     <div>
-        <Button on:click={() => (showModal = true)}>🥫 Buy and sell goods</Button>
+        <Button on:click={() => (showChangeGoods = !showChangeGoods)}>🥫 Buy and sell goods</Button>
         <Button on:click={() => merchant.addDoubloons(1000)}>💰 Magic Money</Button>
     </div>
 
-    <div class="slider-container">
-        {#each Object.keys(tempGoods) as item}
-            <div>
-                <RangeSlider
-                    value={tempGoods[item]}
-                    max={tempGoods[item] + 500}
-                    on:input={e => changeGoods(item, e.target.value)}
-                    label="{goodsInfo[item].icon}
-                    {goodsInfo[item].name} ({tempGoods[item]}
-                    {goodsInfo[item].suffix})" />
-            </div>
-        {/each}
-
-        <div>
-            <p class:warning={tempDoubloons < 0}>Doubloons afterwards: {tempDoubloons} dbl</p>
-        </div>
-
-        <div>
-            <Button on:click={makeTransaction} disabled={tempDoubloons < 0}>Make transaction</Button>
-            <Button on:click={resetGoods}>Reset</Button>
-        </div>
-    </div>
-
-    {#if showModal}
-        <Modal on:close={() => (showModal = false)}>
-            <h3>Buy and sell goods</h3>
-
-            <p>Doubloons: {$merchant.doubloons}</p>
-
-            <Table dynamicWidth={true}>
-                <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Actions</th>
-                </tr>
-
-                {#each Object.keys(goodsInfo) as item}
-                    <tr>
-                        <td>{goodsInfo[item].icon} {goodsInfo[item].name}</td>
-                        <td>{$goods[item]} {goodsInfo[item].suffix}</td>
-                        <td>{goodsInfo[item].price}</td>
-                        <td>
-                            <Button
-                                disabled={goodsInfo[item].price > $merchant.doubloons}
-                                on:click={() => goods.buy(item, 1)}>
-                                +
-                            </Button>
-                            <Button disabled={$goods[item] <= 0} on:click={() => goods.sell(item, 1)}>-</Button>
-                        </td>
-                    </tr>
-                {/each}
-            </Table>
-
-        </Modal>
+    {#if showChangeGoods}
+        <ChangeGoods />
     {/if}
-
 </div>
