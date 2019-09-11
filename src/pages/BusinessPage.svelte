289df340198769ae729ce8cb91659fd40a2971ec
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2019-09-11 16:55:05 +0200

    Ability to rename and sell businesses

--- src/pages/BusinessPage.svelte
+++ src/pages/BusinessPage.svelte
@@ -1,6 +1,7 @@
 <script>
     import { businesses } from '../stores/businesses.js';
     import { game } from '../stores/game';
+    import BusinessActions from '../components/business/BusinessActions.svelte';
 
     $: business = $businesses.find(s => s.id === $game.route.id);
 </script>
@@ -12,6 +13,8 @@
 
     {#if business}
         <h2>Business: {business.name}</h2>
+
+        <BusinessActions {business} />
     {:else}
         <p>Business not found.</p>
     {/if}
