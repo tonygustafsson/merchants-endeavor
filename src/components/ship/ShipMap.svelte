06af14053532cf621ecb2780888e3506623f611a
Tony Gustafsson <mail@tonyg.se>
2019-10-01 15:54:47 +0200

    Use objects and define paths in own object

--- src/components/ship/ShipMap.svelte
+++ src/components/ship/ShipMap.svelte
@@ -7,34 +7,31 @@
     const shipHeight = 48;
     const pixelsPerAnimation = 4;
 
-    const towns = new Map();
-    // English
-    towns.set('charlestown', 'Charles Town');
-    towns.set('bridgetown', 'Bridgetown');
-    towns.set('bermuda', 'Bermuda');
+    const routePaths = {
+        charlestown_bridgetown:
+            'm 1004.8856,362.1155 c 0,0 -12.05335,172.60364 29.0602,243.10195 25.7185,44.09996 123.4052,90.70489 123.4052,90.70489',
+        charlestown_bermuda:
+            'm 1004.8856,362.1155 c 0,0 0,0 3.5307,-96.47393 2.821,-77.08159 126.989,-193.441345 126.989,-193.441345',
+        bermuda_charlestown:
+            'm 1135.4053,72.200225 c 0,0 -103.6062,116.053475 -126.9887,187.959445 -10.516,32.3388 -3.531,101.95583 -3.531,101.95583',
+        bridgetown_charlestown:
+            'm 1157.351,695.92234 c 0,0 -13.5248,-83.56164 -32.5131,-120.62323 -29.3659,-57.31656 -101.4462,-88.76255 -122.0352,-149.78415 -6.75992,-20.03487 2.0829,-63.39946 2.0829,-63.39946'
+    };
 
-    // French
-    towns.set('fortdefrance', 'Fort de France');
-    towns.set('tortuga', 'Tortuga');
-    towns.set('portauprince', 'Port au Prince');
-
-    // Spanish
-    towns.set('havana', 'Havana');
-    towns.set('santiago', 'Santiago');
-    towns.set('sanjuan', 'San Juan');
-
-    // Dutch
-    towns.set('villemstad', 'Villemstad');
-    towns.set('marigo', 'Marigo');
-    towns.set('cartagena', 'Cartagena');
-
-    const routes = {};
-    towns.forEach((townFromName, townFromId) => {
-        towns.forEach((townToName, townToId) => {
-            if (townFromId === townToId) return;
-            routes[`${townFromId}_${townToId}`] = null;
-        });
-    });
+    const towns = [
+        'charlestown',
+        'bridgetown',
+        'bermuda',
+        'fortdefrance',
+        'tortuga',
+        'portauprince',
+        'havana',
+        'santiago',
+        'sanjuan',
+        'villemstad',
+        'marigo',
+        'cartagena'
+    ];
 
     let traveling = false;
 
@@ -45,19 +42,21 @@
     let shipIcon;
 
     const travel = (from, to) => {
-        if (!towns.has(from) || !towns.has(to)) return;
+        if (!towns.includes(from) || !towns.includes(to)) return;
 
-        let routePath = routes[`${from}_${to}`];
+        let routePath = routePaths[`${from}_${to}`];
 
         if (!routePath) return;
 
         traveling = true;
 
-        routePathLength = routePath.getTotalLength();
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttributeNS(null, 'd', routePath);
+        routePathLength = path.getTotalLength();
 
         const animate = () => {
             routePosition += pixelsPerAnimation;
-            const shipPositionPoint = routePath.getPointAtLength(routePosition);
+            const shipPositionPoint = path.getPointAtLength(routePosition);
             const newDirection = shipPosition.x < shipPositionPoint.x - shipWidth / 2 ? 'right' : 'left';
 
             if (shipDirection !== newDirection) {
@@ -97,14 +96,13 @@
 </style>
 
 <div class="ship-map">
-    <button disabled={traveling} on:click={() => travel('charlestown', 'bridgetown')}>
-        Charles Town to Bridgetown
-    </button>
-    <button disabled={traveling} on:click={() => travel('bridgetown', 'charlestown')}>
-        Bridgetown to Charles Town
-    </button>
-    <button disabled={traveling} on:click={() => travel('charlestown', 'bermuda')}>Charles Town to Bermuda</button>
-    <button disabled={traveling} on:click={() => travel('bermuda', 'charlestown')}>Bermuda to Charles Town</button>
+    {#each towns as fromTown}
+        {#each towns as toTown}
+            {#if `${fromTown}_${toTown}` in routePaths}
+                <button disabled={traveling} on:click={() => travel(fromTown, toTown)}>{fromTown} to {toTown}</button>
+            {/if}
+        {/each}
+    {/each}
 
     <svg
         class="map"
@@ -121,28 +119,5 @@
             y={shipPosition.y}
             width={shipWidth}
             height={shipHeight} />
-        <path
-            bind:this={routes.charlestown_bridgetown}
-            id="charles-town-bridgetown"
-            d="m 1004.8856,362.1155 c 0,0 -12.05335,172.60364 29.0602,243.10195 25.7185,44.09996 123.4052,90.70489
-            123.4052,90.70489"
-            style="fill:none;stroke:none" />
-        <path
-            bind:this={routes.charlestown_bermuda}
-            id="charles-town-bermuda"
-            d="m 1004.8856,362.1155 c 0,0 0,0 3.5307,-96.47393 2.821,-77.08159 126.989,-193.441345 126.989,-193.441345"
-            style="fill:none;stroke:none" />
-        <path
-            bind:this={routes.bermuda_charlestown}
-            id="bermuda-charles-town"
-            d="m 1135.4053,72.200225 c 0,0 -103.6062,116.053475 -126.9887,187.959445 -10.516,32.3388 -3.531,101.95583
-            -3.531,101.95583"
-            style="fill:none;stroke:none" />
-        <path
-            bind:this={routes.bridgetown_charlestown}
-            id="bridgedown-charles-town"
-            d="m 1157.351,695.92234 c 0,0 -13.5248,-83.56164 -32.5131,-120.62323 -29.3659,-57.31656 -101.4462,-88.76255
-            -122.0352,-149.78415 -6.75992,-20.03487 2.0829,-63.39946 2.0829,-63.39946"
-            style="fill:none;stroke:none" />
     </svg>
 </div>
