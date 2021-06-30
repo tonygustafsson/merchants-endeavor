503fa1a00ce39daf11ce93dc08b25d5c251d7ac0
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-06-30 17:52:59 +0200

    Trying to make build to work

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +10,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		ssr: false
 	}
 };
